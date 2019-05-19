c3a698f6ea6c7b7139debe8b4be46d3866267e37
Robert D French <robert@robertdfrench.me>
2019-05-19 16:11:53 -0400

    Consolidate naming scheme for terraform resources

--- host.tf
+++ host.tf
@@ -1,15 +1,20 @@
+output "host_ip" {
+  value = "${triton_machine.host.primaryip}"
+}
+
 resource "triton_machine" "host" {
   name        = "doordown"
   package     = "g4-highcpu-128M"
-  networks    = ["${data.triton_network.public.id}"]
-  image       = "${data.triton_image.smartos.id}"
+  networks    = ["${data.triton_network.host.id}"]
+  image       = "${data.triton_image.host.id}"
   user_script = "${file("provision.sh")}"
 }
 
-data "triton_network" "public" {
+data "triton_network" "host" {
   name = "Joyent-SDC-Public"
 }
 
-output "host_ip" {
-  value = "${triton_machine.host.primaryip}"
+data "triton_image" "host" {
+  name    = "base-64"
+  version = "18.3.0"
 }
--- provider.tf
+++
@@ -1,7 +0,0 @@
-variable "provider_account" {}
-variable "provider_key_id" {}
-
-provider "triton" {
-  account = "${var.provider_account}"
-  key_id  = "${var.provider_key_id}"
-}
